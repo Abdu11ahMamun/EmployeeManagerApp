674359216b988ddcfa62e6f6c9d1c363ef21ad09
Abdu11ahMamun <cs.abdullah.mamun@gmail.com>
2023-07-09 17:17:01 +0600

    first angular service done

--- frontend/employeemanagerapp/src/app/employee.ts
+++ frontend/employeemanagerapp/src/app/employee.ts
@@ -1,3 +1,10 @@
-export interface Emplyee {
+export interface Employee  {
     id: number;
+    name: string;
+    email: string;
+    jobTitle: string;
+    phone: string;
+    imageUrl: string;
+    employeeCode: string;
+    
 }
